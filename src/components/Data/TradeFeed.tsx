9f9199b316757de5a9b95a26b52cca0011ee098f
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-04-08 06:04:33 +0530

    [TASK] : Defined the Trade Feed

--- src/components/Data/TradeFeed.tsx
+++ src/components/Data/TradeFeed.tsx
@@ -1,6 +1,22 @@
+import { useSelector } from "react-redux";
+import type { RootState } from "../../store/store";
+import { List, ListItem, Typography } from "@mui/material";
 
-export const TradeFeed = () => {
+const TradeFeed = () => {
+  const trades = useSelector((state: RootState) => state.crypto.trades);
+
   return (
-    <div>TradeFeed</div>
-  )
-}
+    <List sx={{ maxHeight: 300, overflow: "auto" }}>
+      {trades.map((trade, index) => (
+        <ListItem key={index}>
+          <Typography>
+            ${trade.price.toFixed(2)} -{" "}
+            {new Date(trade.time).toLocaleTimeString()}
+          </Typography>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default TradeFeed;
